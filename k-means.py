fcfc3293ee49704c23e074410e79364a06ba1d68
Igor-dJS <igordejesussantos@hotmail.com>
2022-07-23 19:51:13 -0300

    Update k-means.py

--- k-means.py
+++ k-means.py
@@ -1,3 +1,51 @@
+import enum
 import numpy as np
 
-print("Saulo testando")
+def k_means1D(n_clusters, data):
+    data = np.sort(data)
+
+    min = data[0]
+    max = data[data.size - 1]
+
+    centroides = np.array([np.random.randint(min, max) for _ in range(n_clusters)])
+    lista_anterior  = list(centroide)
+    lista = []
+
+    #Consertar erro
+    dicionario = dict(((centroide, set()) for centroide in centroides))
+    print(dicionario)
+
+    for i, ponto in enumerate(data):
+        min_dist = np.inf
+        min_centr = None
+
+        for key in dicionario:
+            if abs(ponto - key) < min_dist:
+                min_dist = ponto - key
+                min_centr = key
+
+        dicionario[min_centr].add(i)
+
+    for k in dicionario:
+        lista.append(sum([data[index] for index in dicionario[k]]) / len(dicionario[k]))
+    
+
+    while(lista_anterior != lista):
+
+    for k in dicionario:
+        lista.append(sum([data[index] for index in dicionario[k]]) / len(dicionario[k]))
+
+
+
+def k_means(n_clusters, data):
+    if len(data.shape) == 1:
+        k_means1D(n_clusters, data)
+    elif data.shape[1] == 2:
+        pass
+    elif data.shape[1] == 3:
+        pass
+    else:
+        print("erro")
+        
+
+k_means(2, np.array([1,2,3,4,10]))
