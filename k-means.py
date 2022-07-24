3059ef77ef5ab046b44175a85658a087ceab8647
saulorcj <sauloroberto1000@hotmail.com>
2022-07-24 15:33:18 -0300

    Algumas otimizações e alterações

--- k-means.py
+++ k-means.py
@@ -1,12 +1,16 @@
 import numpy as np
-import math
 
 
-def k_means1D(n_clusters, data):
+def k_means_1d(n_clusters, data):
+    """
+    :param n_clusters: número de clusters que o usuário deseja obter
+    :param data: array de pontos de 1 dimensão
+    :return: lista de centroides, lista dos conjuntos de pontos de acordo com os centroides
+    """
     centers = None
     data.sort()
 
-    # Evitando que haja centróides repetidos
+    # Evitando que haja centroides repetidos
     cluster_centers = set()
     while len(cluster_centers) != n_clusters:
         cluster_centers = set([np.random.randint(data[0], data[data.size - 1]) for _ in range(n_clusters)])
@@ -20,7 +24,7 @@ def k_means1D(n_clusters, data):
         list_before = list_after
         centers = [set() for _ in range(n_clusters)]
 
-        # Separando os pontos por centróide mais próximo
+        # Separando os pontos por centroide mais próximo
         for point in data:
             min_dist = np.inf
             min_center = None
@@ -32,7 +36,7 @@ def k_means1D(n_clusters, data):
 
             centers[min_center].add(point)
 
-        # Coletando a média dos pontos de cada centróide
+        # Coletando a média dos pontos de cada centroide
         list_after = []
         for center in centers:
             qtd_points = len(center)
@@ -41,92 +45,95 @@ def k_means1D(n_clusters, data):
             else:
                 list_after.append(None)
 
-    return centers
+    # TODO: mudar return para dicionário de arrays
+    return list_after, centers
+
 
 def geraPonto(x_lim, y_lim):
-  eixo_x = np.random.randint(x_lim[0], x_lim[1])
-  eixo_y = np.random.randint(y_lim[0], y_lim[1])
+    eixo_x = np.random.randint(x_lim[0], x_lim[1])
+    eixo_y = np.random.randint(y_lim[0], y_lim[1])
+    return [eixo_x, eixo_y]
 
-  return [eixo_x, eixo_y]
 
-def calculaDist(p1, p2):
-  return math.sqrt( (p1[0] - p2[0])**2 + (p1[1] - p2[1])**2 )
+def calcula_dist(p1, p2):
+    """
+    :param p1: primeiro ponto (float x, float y)
+    :param p2: segundo ponto (float x, float y)
+    :return: distância entre o primeiro e segundo ponto
+    """
+    return ((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2) ** (1 / 2)
+
 
 def geraPonto3D(x_lim, y_lim, z_lim):
-  eixo_x = np.random.randint(x_lim[0], x_lim[1])
-  eixo_y = np.random.randint(y_lim[0], y_lim[1])
-  eixo_z = np.random.randint(z_lim[0], z_lim[1])
+    eixo_x = np.random.randint(x_lim[0], x_lim[1])
+    eixo_y = np.random.randint(y_lim[0], y_lim[1])
+    eixo_z = np.random.randint(z_lim[0], z_lim[1])
+    return [eixo_x, eixo_y, eixo_z]
 
-  return [eixo_x, eixo_y, eixo_z]
 
 def calculaDist3D(p1, p2):
-  return math.sqrt( (p1[0] - p2[0])**2 + (p1[1] - p2[1])**2 + (p1[2] - p2[2])**2)
+    return ((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2 + (p1[2] - p2[2])**2) ** (1 / 2)
+
 
 def k_means2D(n_clusters, data):
-    qtd_pontos = data.shape[0]
     pontos_X = data[:, 0]
     pontos_Y = data[:, 1]
 
-    ponto_X_ord = np.sort(pontos_X)
-    ponto_Y_ord = np.sort(pontos_Y)
-
-    x_min = ponto_X_ord[0]
-    y_min = ponto_Y_ord[0]
-    x_max = ponto_X_ord[qtd_pontos - 1]
-    y_max = ponto_Y_ord[qtd_pontos - 1]
-
-    centroides = np.array([geraPonto((x_min, x_max), (y_min, y_max)) for _ in range(n_clusters)])
+    centroides = np.array([geraPonto((min(pontos_X), max(pontos_X)),
+                                     (min(pontos_Y), max(pontos_Y)))
+                           for _ in range(n_clusters)])
+    # Lista dos centroides mais próximos
     lista_lnk = []
-
+    print("centroides", centroides)
     for ponto in data:
-      cent = 0
-      dist = calculaDist(ponto, centroides[0])
+        cent_min = 0
+        dist_min = np.inf
 
-      for k in range(1,len(centroides)):
-        new_dist = calculaDist(ponto, centroides[k])
-        if new_dist < dist:
-          cent = k
-          dist = new_dist
-
-      lista_lnk.append(cent)
-
+        for i, centroide in enumerate(centroides):
+            new_dist = calcula_dist(ponto, centroide)
+            if new_dist < dist_min:
+                cent_min = i
+                dist_min = new_dist
+        lista_lnk.append(cent_min)
+    print("lista_lnk", lista_lnk)
     lista_anterior = None
-    
+
     while lista_anterior != lista_lnk:
-      lista_anterior = lista_lnk
+        lista_anterior = lista_lnk
 
-      #Inicializando arrays locais qeu vão guardar as soma dos eixos dos
-      #centroides e a qauntidade de vezes que aparecem
-      cent_x = np.zeros(len(centroides))
-      cent_y = np.zeros(len(centroides))
-      qtde_cent = np.zeros(len(centroides))
+        # Inicializando arrays locais qeu vão guardar as soma dos eixos dos
+        # centroides e a qauntidade de vezes que aparecem
+        cent_x = np.zeros(len(centroides))
+        cent_y = np.zeros(len(centroides))
+        qtde_cent = np.zeros(len(centroides))
 
-      #Somando nos eixos dos centroides e add um para cada vez que aparece na
-      #lista
-      for i,n in enumerate(lista_lnk):
-        cent_x[n] += pontos_X[i]
-        cent_y[n] += pontos_Y[i]
-        qtde_cent[n] += 1
+        # Somando nos eixos dos centroides e add um para cada vez que aparece na
+        # lista
+        for i,n in enumerate(lista_lnk):
+            cent_x[n] += pontos_X[i]
+            cent_y[n] += pontos_Y[i]
+            qtde_cent[n] += 1
 
-      #Atualizando os novo centroides
-      for k in range(len(centroides)):
-        centroides[k] = [round(cent_x[k] / qtde_cent[k], 5), round(cent_y[k] / qtde_cent[k], 5)]
+        # Atualizando os novo centroides
+        for k in range(len(centroides)):
+            centroides[k] = [round(cent_x[k] / qtde_cent[k], 5), round(cent_y[k] / qtde_cent[k], 5)]
 
-      lista_lnk = []
-      #Verificando a qual dos novos centroides os pontos pertecncem agora
-      for i,ponto in enumerate(data):
-        cent = 0
-        dist = calculaDist(ponto, centroides[0])
+        lista_lnk = []
+        # Verificando a qual dos novos centroides os pontos pertecncem agora
+        for i,ponto in enumerate(data):
+            cent = 0
+            dist = calcula_dist(ponto, centroides[0])
 
-        for k in range(1,len(centroides)):
-          new_dist = calculaDist(ponto, centroides[k])
-          if new_dist < dist:
-            cent = k
-            dist = new_dist
+            for k in range(1,len(centroides)):
+                new_dist = calcula_dist(ponto, centroides[k])
+                if new_dist < dist:
+                    cent = k
+                    dist = new_dist
 
-        lista_lnk.append(cent)
+            lista_lnk.append(cent)
 
     return lista_lnk, centroides
+
 
 def k_means3D(n_clusters, data):
     qtd_pontos = data.shape[0]
@@ -201,9 +208,10 @@ def k_means3D(n_clusters, data):
 
     return lista_lnk, centroides
 
+
 def k_means(n_clusters, data):
     if len(data.shape) == 1:
-        return k_means1D(n_clusters, data)
+        return k_means_1d(n_clusters, data)
     elif data.shape[1] == 2:
         return k_means2D(n_clusters, data)
     elif data.shape[1] == 3:
@@ -212,5 +220,7 @@ def k_means(n_clusters, data):
         return None
         
 
-resultado = k_means(3, np.array([1, 2, 3, 4, 10, 15, 20, 100, 155, 200]))
+# resultado = k_means(3, np.array([1, 2, 3, 4, 10, 15, 20, 100, 155, 200]))
+resultado = k_means(3, np.array([[1, 2], [7, 11], [100, 1], [200, 4], [0, 0], [27, 33]]))
+
 print(resultado)
