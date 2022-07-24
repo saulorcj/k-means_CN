72be4162aeb65c33dd20efd79be8595b4d1d1bad
saulorcj <saulorcj@dcc.ufrj.br>
2022-07-23 22:25:22 -0300

    k-means com 1 dimensão finalizado

--- k-means.py
+++ k-means.py
@@ -2,44 +2,51 @@ import numpy as np
 
 
 def k_means1D(n_clusters, data):
+    points_by_center = None
+
     data.sort()
 
+    # Evitando que haja centróides repetidos
     cluster_centers = set()
-    while len(cluster_centers) != 2:
+    while len(cluster_centers) != n_clusters:
         cluster_centers = set([np.random.randint(data[0], data[data.size - 1]) for _ in range(n_clusters)])
-    cluster_centers = np.array(cluster_centers)
+    cluster_centers = np.array(list(cluster_centers))
 
     lista_anterior = None
     lista_atual = []
 
-    dicionario = dict(((centroide, set()) for centroide in centroides))
-    print(dicionario)
+    while lista_anterior != lista_atual:
+        lista_anterior = lista_atual
+        points_by_center = dict(zip(cluster_centers, [set() for _ in range(n_clusters)]))
 
-    for i, ponto in enumerate(data):
-        min_dist = np.inf
-        min_centr = None
+        # Separando os pontos por centróide mais próximo
+        for i, point in enumerate(data):
+            min_dist = np.inf
+            min_center = None
+            for center in points_by_center:
+                dist = abs(point - center)
+                if dist < min_dist:
+                    min_dist = dist
+                    min_center = center
 
-        for key in dicionario:
-            if abs(ponto - key) < min_dist:
-                min_dist = ponto - key
-                min_centr = key
+            points_by_center[min_center].add(i)
 
-        dicionario[min_centr].add(i)
+        # Coletando a média dos pontos de cada centróide
+        lista_atual = []
+        for center in points_by_center:
+            sum_points = sum([data[index] for index in points_by_center[center]])
+            qtd_points = len(points_by_center[center])
+            if qtd_points:
+                lista_atual.append(round(sum_points / qtd_points, 5))
+            else:
+                lista_atual.append(None)
 
-    for k in dicionario:
-        lista.append(sum([data[index] for index in dicionario[k]]) / len(dicionario[k]))
-    
-    while lista_anterior != lista:
-        pass
-
-    for k in dicionario:
-        lista.append(sum([data[index] for index in dicionario[k]]) / len(dicionario[k]))
-
+    return points_by_center
 
 
 def k_means(n_clusters, data):
     if len(data.shape) == 1:
-        k_means1D(n_clusters, data)
+        print(k_means1D(n_clusters, data))
     elif data.shape[1] == 2:
         pass
     elif data.shape[1] == 3:
@@ -48,4 +55,4 @@ def k_means(n_clusters, data):
         print("erro")
         
 
-k_means(2, np.array([1,2,3,4,10]))
+k_means(3, np.array([1, 2, 3, 4, 10, 15, 20, 100, 155, 200]))
