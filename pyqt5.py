a659a5d26782f2ab0d11191ae072a7fb34c6a0c7
Igor-dJS <igordejesussantos@hotmail.com>
2022-07-24 21:18:34 -0300

    Add arquivo de interface gráfica

---
+++ pyqt5.py
@@ -0,0 +1,41 @@
+import sys
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from k_means import *
+from PyQt5.QtWidgets import QApplication,QWidget,QInputDialog,QLineEdit
+from PyQt5.QtGui import QIcon
+
+class App(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.title='K-mean clustering'
+        self.left=10
+        self.top=10
+        self.width=300
+        self.height=250
+        self.initUI()
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left,self.top,self.width,self.height)
+        self.getText()
+        self.show()
+    def getText(self):
+        d1,okPressed1=QInputDialog.getText(self, "Arquivo", "Caminho")
+        d2,okPressed2=QInputDialog.getInt(self, "Centroides", "Qtde:")
+        if okPressed1 and okPressed2:
+            df = pd.read_csv(d1, delimiter="," )
+
+            data = df.iloc[:,[5,7]].to_numpy()
+
+            a, b = k_means(d2, data)
+            
+            plt.grid("True")
+            plt.scatter(data[:,0], data[:,1], c = a)
+            plt.scatter(b[:,0], b[:,1], s = 70, c = 'red')
+            plt.show()
+
+
+if __name__=='__main__':
+    app=QApplication(sys.argv)
+    ex=App()
